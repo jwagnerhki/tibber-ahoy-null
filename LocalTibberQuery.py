46e092675ca661a877c3f23f8e45220bed2fe657
jwagner <jan.wagner@iki.fi>
2023-09-07 10:59:53 +0200

    reworded docu in LocalTibberQuery.py

--- LocalTibberQuery.py
+++ LocalTibberQuery.py
@@ -11,25 +11,24 @@
 # by trial and error, or by some reverse engineering with https://github.com/volkszaehler/libsml
 #
 # Tibber readout assumes the web interface has been enabled on the Tibber Bridge.
-# Steps to enable it are explained on other web sites.
-# Steps essentially are essentially there:
-#   1) configure Tibber Pulse normally via App, pair with Tibber
+# Steps to enable it are explained on other web sites,
+# here a copy in brief:
+#   1) configure your Tibber Pulse normally via App, pair with Tibber
 #   2) note down the typ. 8-letter password (XXXX-XXXX) found printed
 #      on the Tibber Bridge device near its QR code label
-#   3) power cycle the Pulse, wait till green light, power cycle again,
+#   3) power cycle the Pulse, wait till a green light, power cycle again,
 #      wait till blue light and then search for wireless network "Tibber Bridge"
-#   4) connect to wifi network "Tibber Bridge", password as in (2)
+#   4) connect to wifi network "Tibber Bridge", wifi password as in (2)
 #   5) open the Bridge's page http://10.133.70.1/params/, user 'admin', password as in (2)
-#   6) find the entry called 'webserver_force_enable', change
-#      the value to 'true' and Save it
-#   7) find near the end fo the page 'Write settings to flash' or similar
-#      and have the Bridge do that
-#   8) change to http://10.133.70.1/console/ and enter 'reboot', or,
-#      just manually power cycle the Bridge
-#   9) after booting again the Bridge ought to be back on the
-#      normal wifi network it was added to during (1)
+#   6) find the entry called 'webserver_force_enable',
+#      change the value to 'true', then click the 'Save' button to store the setting
+#   7) click the button 'Write settings to flash' or similar near the end of the page
+#   8) open the page http://10.133.70.1/console/, enter 'reboot',
+#      alternatively just manually power cycle the Bridge
+#   9) after rebooting, the Bridge ought to be back on the
+#      your normal wifi network that it was added to during step (1)
 #   10) figure out the local IP or address of your Bridge on
-#      the normal wifi network make sure you can connect
+#      the normal wifi network, then make sure you can connect
 #      to the Bridge at http://<bridge ip on your wifi>/params/
 #
 # The local Bridge Web interface provides only the raw data (SML frames)
