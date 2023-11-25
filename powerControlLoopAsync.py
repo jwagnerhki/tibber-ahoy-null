74a050ec289b279f862ed4a538f323b8a7d4088c
atom <atom@debian-BULLSEYE-live-builder-AMD64>
2023-11-25 23:52:57 +0100

    catch more exceptions, add Off during AC Charge -priority

--- powerControlLoopAsync.py
+++ powerControlLoopAsync.py
@@ -67,7 +67,7 @@ recheck_interval_s = 10           # Interval at which to query for new values
 lfp_undervoltage = 51.3           # DC safety limit, turn off the inverter altogether then the input voltage drops to this level
 lfp_recovery_voltage = 52.3       # DC recovery limit, restart inverter once undervoltage has cleared e.g. battery charged sufficiently
 				  # Note, 16S LFP voltages approx.: 51.2V = 20%, 52.0 = 40%, 52.3 = 60%, 53.1 = 80% charged
-lfp_min_SOC_percent = 10.0        # SOC safety limit, turn off inverter when remaining charge of battery is below 20%
+lfp_min_SOC_percent = 20.0        # SOC safety limit, turn off inverter when remaining charge of battery drops to this level
 lfp_recovery_SOC_percent = 30.0   # SOC recovery limit, restart after charged sufficiently _and_ lfp_recovery_voltage is met
 
 
@@ -120,16 +120,19 @@ async def query_steca_mystrom_on(host):
 
 	ac_input_on = False
 	url = 'http://%s/report' % (host)
-	async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=5)) as client:
-		async with client.get(url) as resp:
-			if resp.status != 200:
-				print ('Failed to query %s' % (url))
-				return ac_input_on
-			j = await resp.json()
-			ac_input_on = (j['relay'] == True)
-			return ac_input_on
 
-	print('Unexpected result of querying %s, perhaps cannot connect' % (url))
+	try:
+		async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=5)) as client:
+			async with client.get(url) as resp:
+				if resp.status != 200:
+					print ('Failed to query %s' % (url))
+					return ac_input_on
+				j = await resp.json()
+				ac_input_on = (j['relay'] == True)
+				return ac_input_on
+
+	except Exception as e:
+		print('Unexpected result of querying %s - exception: %s' % (url, str(e)))
 
 	return ac_input_on
 
@@ -164,27 +167,33 @@ async def controlLoop():
 		else:
 			dynamic_max_power_W = inverter_night_max_power_W
 
+		#timing0 = time.perf_counter()
 		[invdata,gridsml,bmsVolt,bmsSOC,stecaCharge] = await asyncio.gather(*[dtu.getInverterReadings(),
 			meter.getMeterSMLFrame(),
 			bms.getBatteryVoltage(), bms.getBatteryPercentage(),
 			query_steca_mystrom_on(steca_ac_host)
 		])
+		#dtiming = time.perf_counter() - timing0
+		#print('Network wait time (ms):', 1e3*dtiming) # approx 150ms..250ms, vs non-async ~600ms
 
 		print()
-		print('Local time       : ', Tloc)
+		print('Local time       : %s' % (str(Tloc)))
 
 		print('Steca AC In      : %s' % ('ON' if stecaCharge else 'off'))
 
 		if 'U_DC' in invdata and 'P_AC' in invdata and float(invdata['U_DC']) > 0:
-			print('DC input voltage : %6.2f V_dc' % (invdata['U_DC']))
-			print('AC output power  : %6.2f W_rms of max %d W' % (invdata['P_AC'], dynamic_max_power_W))
+			print('DC input voltage : %.2f V_dc' % (invdata['U_DC']))
+			print('AC output power  : %.2f W_rms of max %d W' % (invdata['P_AC'], dynamic_max_power_W))
 			dtu_T = T
 			dtu_P = invdata['P_AC']
+			dtu_V = float(invdata['U_DC'])
+		else:
+			dtu_V, dtu_P = 0.0, 0.0
 
 		if bmsVolt > 0:
 			print('Battery voltage  : %.2f V per BMS' % (bmsVolt))
-		if bmsSOC > 0:
-			print('Battery remain   : %3.0f %%' % (bmsSOC))
+		if bmsSOC > 0 or True:
+			print('Battery remain   : %.0f %%' % (bmsSOC))
 
 
 		if gridsml and len(gridsml) > 0:
@@ -201,22 +210,23 @@ async def controlLoop():
 				mqtt_submit(mqtt_logger_host, mqtt_tibber_E_topic, int(meter_E))
 
 
-		# Check battery undervoltage & low charge remaining, and recovery from it
-		if 'U_DC' in invdata and float(invdata['U_DC']) > 0:
-			if invdata['U_DC'] <= lfp_undervoltage or (bmsSOC > 0 and bmsSOC <= lfp_min_SOC_percent):
-				hitUndervoltage = True
-			elif invdata['U_DC'] >= lfp_recovery_voltage and bmsSOC >= lfp_recovery_SOC_percent:
-				hitUndervoltage = False
-
 		# Stop microinverter if Steca Solarix hybrid inverter AC-IN was
 		# switched on (outside of this script) to charge battery esp.
 		# at night during a minimal electricity cost hour (Tibber); no point
 		# in microinverter feeding house via Solarix-internal battery charger...
-		if stecaCharge and 'P_AC' in invdata and float(invdata['P_AC']) > 0:
+		if stecaCharge:  # and ('P_AC' in invdata and float(invdata['P_AC']) > 0):
 			print("Command power    : OFF due to Steca Solarix hybrid inverter charging battery from AC In")
 			command_power_state(ahoydtu_host, ahoydtu_inverterId, powerEnabled=False)
 			time.sleep(settling_time_s)
 			continue
+
+		# Check battery undervoltage & low charge remaining, and recovery from it
+		if 'U_DC' in invdata and dtu_V > 0:
+			#if dtu_V <= lfp_undervoltage:
+			if dtu_V <= lfp_undervoltage or bmsSOC <= lfp_min_SOC_percent:
+				hitUndervoltage = True
+			elif dtu_V >= lfp_recovery_voltage and bmsSOC >= lfp_recovery_SOC_percent and not stecaCharge:
+				hitUndervoltage = False
 
 		# During undervoltage, shut down the u-inverter power production,
 		# turn back on only after undervoltage condition has cleared
@@ -225,7 +235,7 @@ async def controlLoop():
 			command_power_state(ahoydtu_host, ahoydtu_inverterId, powerEnabled=False)
 			time.sleep(settling_time_s)
 			continue
-		elif (not hitUndervoltage) and 'P_AC' in invdata and float(invdata['P_AC']) <= 0:
+		elif (not hitUndervoltage) and 'P_AC' in invdata and float(invdata['P_AC']) <= 0 and not stecaCharge:
 			print("Command power    : ON due to recovery from earlier DC undervoltage or AC-Charge Priority")
 			command_power_state(ahoydtu_host, ahoydtu_inverterId, powerEnabled=True)
 			time.sleep(settling_time_s)
@@ -257,6 +267,8 @@ async def controlLoop():
 					prev_adjust_T = T
 				else:
 					print("Future cmd power : %d Watt" % (new_P))
+		else:
+			print('Not enough recent data in this interval, skipping adjustments')
 
 		# Pause until the next iteration
 		tsleep = recheck_interval_s - (datetime.datetime.utcnow() - T).seconds
@@ -267,4 +279,9 @@ async def controlLoop():
 
 if __name__ == '__main__':
 
-	asyncio.run(controlLoop())
+	while True:
+		try:
+			asyncio.run(controlLoop())
+		except Exception as e:
+			print('Main program ran into an exception: %s' % (str(e)))
+			print('Plowing on, anyway...')
